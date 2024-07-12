8e9ee86986c86fc56fdb203e2a8f7ec0c9776988
Omaan Mallick <149195526+OmaanMallick@users.noreply.github.com>
2024-07-12 20:46:36 +0530

    Update FooterCircles.jsx

--- src/components/FooterCircles.jsx
+++ src/components/FooterCircles.jsx
@@ -1,11 +1,10 @@
-// src/FooterCircles.js
 import React from 'react';
 import './FooterCircles.css';
 
 const circles = [
-  { id: 'circle1', text: 'GL', hoverText: 'Text for Circle 1' },
-  { id: 'circle2', text: 'ZA Cafe', hoverText: 'Text for Circle 2' },
-  { id: 'circle3', text: 'Dubai', hoverText: 'Text for Circle 3' },
+  { id: 'circle1', hoverText: 'Bloomburrow Pre-release is coming soon! Continuing with Monday nights as everyone really enjoyed the extra day if the weekend ended up being busy. Enjoy the cutest set of the year!' },
+  { id: 'circle2', hoverText: 'Come on in with your friends and family and play boardgames from our collection of over 500+ games! Just $10 a person for 3 hours of gameplay' },
+  { id: 'circle3', hoverText: 'Want to play the new Disney trading card game? Were hosting a Lorcana learn to play event on July 22nd 6pm! Just grab a starter deck at Sip & Play and well have players on hand to teach you.' }
 ];
 
 const FooterCircles = () => {
